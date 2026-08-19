a5efecc17bfe06aa8447af22346c4d9fc499348d
彭星源 <13356179810@163.com>
2026-08-19 08:53:32 +0800

    更新 navbar

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -19,35 +19,17 @@ export const navbar = defineNavbarConfig([
     text: '电子取证', link: '/Forensics/'
   },
   {
-    icon: 'twemoji:brain',
-    text: '公专', link: '/ncsec/avrj0bx2/'
-  },
-  {
     icon: 'streamline-color:chat-bubble-square-write',
-    text: '申论', link: '/ncsec/cf2nuwcv/'
-  },
-  {
-    icon: 'fluent-color:data-area-32',
-    text: '行测', link: '/ncsec/evqrjpxi/'
-  },
-  {
-    icon: 'mdi:history',
-    text: '前车之鉴', link: '/ncsec/3nqfrefd/'
-  },
-  {
-    icon: 'mdi:history',
-    text: '面试冲刺', link: '/ncsec/paectakp/'
+    text: '公考', link: '/ncsec/'
   },
   {
     icon: 'fluent-color:notebook-16',
     text: '笔记',
     items: [
       { text: 'Menu', link: '/menu/' },
-      { text: 'Forensics', link: '/Forensics/' },
       { text: 'CTFShow', link: '/ctfshow/' },
       { text: 'Paper', link: '/paper/' },
       { text: '渗透专题整理', link: '/pentest/' },
-      { text: '公安联考', link: '/ncsec/' },
     ],
   },
 
